9c41286bc5f2a2358038aa879ac9101a3e1ac5c5
Tesfachin <tesfachingizaw@gmail.com>
2019-04-18 16:09:37 -0400

    comment on home controller

--- FinalizingCheck/Controllers/HomeController.cs
+++ FinalizingCheck/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace FinalizingCheck.Controllers
 {
+    //There is nothing to change on you
     public class HomeController : Controller
     {
         public ActionResult Index()
